dda6a33b48918e900c527f71da7cbdd86336c872
Himanshu Kumar <mk.himanshu5252@gmail.com>
2024-03-30 17:28:38 +0530

    ep-01

---
+++ app.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div",{id:"parent"},
+[React.createElement("div",{id:"child"},
+[React.createElement("h1",{},"hello himanshhhhu"),
+React.createElement("h2",{},"good to see you again")])
+,React.createElement("div",{id:"child"},
+[React.createElement("h1",{},"hello himanshhhhu"),
+React.createElement("h2",{},"good to see you again")])
+]);
+
+const heading = React.createElement("h1",{id:"heading"},"hello himanshu are you ready for learning REACT");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
